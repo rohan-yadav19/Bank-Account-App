a985ccf4c540661279e3d5e2a8323e03cd706acd
Rohan Yadav <yadavrohan98040@gmail.com>
2025-05-09 21:04:18 +0530

    update get balance

--- index.js
+++ index.js
@@ -1,22 +1,71 @@
-var BankAccount = /** @class */ (function () {
-  function BankAccount() {
-    this._balance = 0;
-  }
-  Object.defineProperty(BankAccount.prototype, "balance", {
-    get: function () {
-      return this._balance;
-    },
-    set: function (newBalance) {
-      if (newBalance < 0) {
-        throw new Error("Invalid Balance");
-      }
-      this._balance = newBalance;
-    },
-    enumerable: false,
-    configurable: true,
-  });
-  return BankAccount;
-})();
-var account = new BankAccount();
-account.balance = 10;
-console.log(account.balance);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BankAccount = void 0;
+class BankAccount {
+    balance;
+    accountNumber;
+    accountHolder;
+    transactions;
+    constructor(accountNumber, accountHolder, initialBalance = 0) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = initialBalance;
+        this.transactions = [];
+    }
+    deposit(amount) {
+        if (amount <= 0) {
+            throw new Error("Deposit amount must be positive");
+        }
+        this.balance += amount;
+        this.transactions.push({
+            type: "deposit",
+            amount: amount,
+            timestamp: new Date(),
+            balance: this.balance
+        });
+    }
+    withdraw(amount) {
+        if (amount <= 0) {
+            throw new Error("Withdrawal amount must be positive");
+        }
+        if (amount > this.balance) {
+            throw new Error("Insufficient funds");
+        }
+        this.balance -= amount;
+        this.transactions.push({
+            type: "withdrawal",
+            amount: amount,
+            timestamp: new Date(),
+            balance: this.balance
+        });
+    }
+    getBalance() {
+        return this.balance;
+    }
+    getAccountNumber() {
+        return this.accountNumber;
+    }
+    getAccountHolder() {
+        return this.accountHolder;
+    }
+    getTransactionHistory() {
+        return [...this.transactions];
+    }
+}
+exports.BankAccount = BankAccount;
+// Example usage
+const account = new BankAccount("1234567890", "John Doe", 1000);
+console.log(`Initial balance: $${account.getBalance()}`);
+try {
+    account.deposit(500);
+    console.log(`After deposit: $${account.getBalance()}`);
+    account.withdraw(200);
+    console.log(`After withdrawal: $${account.getBalance()}`);
+    console.log("\nTransaction History:");
+    account.getTransactionHistory().forEach(transaction => {
+        console.log(`${transaction.type}: $${transaction.amount} at ${transaction.timestamp}`);
+    });
+}
+catch (error) {
+    console.error("Error:", error.message);
+}
